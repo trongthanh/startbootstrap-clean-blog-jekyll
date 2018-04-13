871bb9314bc710bdb1a26d78832182a53456d2f7
Thanh Tran <trongthanh@gmail.com>
2018-04-14 01:25:11 +0700

    Slightly enhance header bg gradient so it's easier to read on light cover photo

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,7 +35,7 @@ module.exports = function(grunt) {
         banner: '/*!\n' +
             ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
             ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-            ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n' +
+            ' * Licensed under <%= pkg.license %>\n' +
             ' */\n',
         usebanner: {
             dist: {
